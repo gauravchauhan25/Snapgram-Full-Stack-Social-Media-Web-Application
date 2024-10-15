d3b0f07e064758cea4cf5bd88a0c9e0f07013f06
Gaurav Singh Chauhan <gkumarc121@gmail.com>
2024-10-15 17:25:56 +0000

    Updates Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import profilePhoto from "../img/profile-photo.jpg";
+import Post from "../components/Posts";
 
 const Profile = () => {
   const user = {
@@ -20,20 +21,46 @@ const Profile = () => {
   };
 
   return (
-    <div className="center profile-container">
-      <div className="profile-header">
-        <div className="profile-picture">
-          <img src={profilePhoto} alt={`${user.username}'s profile`} />
-        </div>
-
-        <div className="profile-info">
-          <div className="edit-profile">
-            <h4>{user.fullName}</h4>
-            <button className="btn btn-white">Edit Profile</button>
+    <>
+      <div className="center profile-container">
+        <div className="profile-header">
+          <div className="profile-picture">
+            <img src={profilePhoto} alt={`${user.username}'s profile`} />
           </div>
 
-          {/* <p>{user.bio}</p> */}
-          <p dangerouslySetInnerHTML={{ __html: user.bio.replace(/,/g, ',<br />') }}></p>
+          <div className="profile-info">
+            <div className="edit-profile">
+              <h4>{user.fullName}</h4>
+              <button className="btn btn-primary">Follow</button>
+            </div>
+
+            <div className="profile-description large">
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: user.bio.replace(/,/g, ",<br />"),
+                }}
+              ></p>
+              <div className="follower-info">
+                <span>
+                  <strong>{user.posts.length}</strong> Posts
+                </span>
+                <span>
+                  <strong>{user.followers}</strong> Followers
+                </span>
+                <span>
+                  <strong>{user.following}</strong> Following
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="profile-description small">
+          <p
+            dangerouslySetInnerHTML={{
+              __html: user.bio.replace(/,/g, ",<br />"),
+            }}
+          ></p>
           <div className="follower-info">
             <span>
               <strong>{user.posts.length}</strong> Posts
@@ -47,14 +74,9 @@ const Profile = () => {
           </div>
         </div>
       </div>
-      {/* <div className="profile-posts">
-        {user.posts.map((post, index) => (
-          <div key={index} className="post">
-            <img src={post} alt={`Post ${index + 1}`} />
-          </div>
-        ))}
-      </div> */}
-    </div>
+
+      {/* <Post /> */}
+    </>
   );
 };
 
